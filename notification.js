e45d50b9ba28bfaefa24da9e771ec25bb41d3531
Rodrigo Espinosa de los Monteros <1084688+RodEsp@users.noreply.github.com>
2021-05-06 17:37:35 -0400

    Use node-fetch because handling redirects with node's https is hard

--- notification.js
+++ notification.js
@@ -1,75 +1,83 @@
-const https = require('https');
+const fetch = require('node-fetch');
 
 const authorIconUrl = process.env.PULL_REQUEST_AUTHOR_ICON_URL;
 const authorName = process.env.PULL_REQUEST_AUTHOR_NAME;
+const authorProfileUrl = process.env.PULL_REQUEST_AUTHOR_PROFILE_URL;
+const compareBranchName = process.env.PULL_REQUEST_COMPARE_BRANCH_NAME;
+const baseBranchName = process.env.PULL_REQUEST_BASE_BRANCH_NAME;
 const PRNum = process.env.PULL_REQUEST_NUMBER;
 const PRRepo = process.env.PULL_REQUEST_REPO;
 const PRTitle = process.env.PULL_REQUEST_TITLE;
 const PRUrl = process.env.PULL_REQUEST_URL;
-const webhookUrl = new URL(process.env.WEBHOOK_URL);
+const webhookUrl = process.env.WEBHOOK_URL;
 
-const message = JSON.stringify({
+const message = {
+    type: 'home',
     blocks: [
+        {
+            type: 'section',
+            fields: [
+                {
+                    type: 'mrkdwn',
+                    text: `*<${PRUrl}|#${PRNum} ${PRTitle}>*`
+                }
+            ]
+        },
+        {
+			"type": "divider"
+		},
         {   
-            type: "section",
+            type: 'section',
             text: {
-                type: "plain_text",
+                type: 'plain_text',
                 text: PRRepo
             }
         },
-        {
-            type: "header",
-            text: {
-                type: "mrkdwn",
-                text: "*<" + PRUrl + "|" + PRTitle + ">*"
-            }
-        },
         {   
-            type: "context",
+            type: 'context',
             elements: [
                 {
-                    type: "image",
+                    type: 'plain_text',
+                    text: baseBranchName
+                },
+                {
+					"type": "plain_text",
+					"text": ":arrow_left:",
+					"emoji": true
+				},
+                {
+                    type: 'plain_text',
+                    text: compareBranchName
+                }
+            ]
+        },
+        {   
+            type: 'context',
+            elements: [
+                {
+                    type: 'mrkdwn',
+                    text: '*Author*'
+                },
+                {
+                    type: 'image',
                     image_url: authorIconUrl,
-                    alt_text: "GitHub Icon",
+                    alt_text: 'GitHub Icon'
                 },
                 {
-                    type: "mrkdwn",
-                    text: "*Author*\n" + authorName,
-                },
-                {
-                    type: "mrkdwn",
-                    text: "*Pull request number*\n#" + PRNum,
+                    type: 'mrkdwn',
+                    text: `<${authorProfileUrl}|${authorName}>`
                 }
             ]
         }
     ]
-});
+};
 
-const options = {
-    hostname: webhookUrl.host,
-    path: webhookUrl.path,
-    method: 'POST',
-    followAllRedirects: true,
-    headers: {
-      'Content-Type': 'application/json',
-      'Content-Length': message.length,
-    },
+if (authorName !== 'SF-CLI-BOT') {
+    fetch(webhookUrl, {
+        method: 'post',
+        body: JSON.stringify(message),
+        headers: {'Content-Type': 'application/json'}
+    })
 }
 
-const req = https.request(options,
-    (res) => {
-        console.log(`Status ${res.statusCode}: Notification sent`);
-        console.log(res.headers);
-
-        res.on('data', (data) => {
-            process.stdout.write(data);
-        })
-    }
-);
-
-req.on('error', (error) => {
-    console.log(error);
-})
-
-req.write(message);
-req.end();
+console.log(message);
